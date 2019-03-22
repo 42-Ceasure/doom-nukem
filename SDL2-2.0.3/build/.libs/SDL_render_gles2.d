9f0d2aff84795eaec64b16af1dc8516f3f888a4f
Cesar GLAVIEUX <cglavieu@e1r3p8.42.fr>
2019-03-22 19:20:59 +0100

    adding of a debug calculating function

---
+++ SDL2-2.0.3/build/.libs/SDL_render_gles2.d
@@ -0,0 +1,7 @@
+build/SDL_render_gles2.lo: \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/render/opengles2/SDL_render_gles2.c \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/render/opengles2/../../SDL_internal.h \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/render/opengles2/../../dynapi/SDL_dynapi.h \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/render/opengles2/../../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h
---
+++ SDL2-2.0.3/build/SDL_rwopsbundlesupport.d
@@ -0,0 +1,8 @@
+build/SDL_rwopsbundlesupport.lo: \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/file/cocoa/SDL_rwopsbundlesupport.m \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/file/cocoa/../../SDL_internal.h \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/file/cocoa/../../dynapi/SDL_dynapi.h \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/file/cocoa/../../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h \
+  /Users/cglavieu/Code/DooM-Nukem-3D/project/SDL2-2.0.3/src/file/cocoa/SDL_rwopsbundlesupport.h
