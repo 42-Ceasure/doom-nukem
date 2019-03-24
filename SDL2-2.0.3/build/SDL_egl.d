220ac377eda1b8cf0b4e30b2f6a49bf3f7ad95e2
42-Ceasure <cglavieu@student.42.fr>
2019-03-24 09:08:56 +0100

    -

--- SDL2-2.0.3/build/SDL_egl.d
+++
@@ -1,7 +0,0 @@
-build/SDL_egl.lo: \
-  /Users/cglavieu/Code/doom-nukem42/SDL2-2.0.3/src/video/SDL_egl.c \
-  /Users/cglavieu/Code/doom-nukem42/SDL2-2.0.3/src/video/../SDL_internal.h \
-  /Users/cglavieu/Code/doom-nukem42/SDL2-2.0.3/src/video/../dynapi/SDL_dynapi.h \
-  /Users/cglavieu/Code/doom-nukem42/SDL2-2.0.3/src/video/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
