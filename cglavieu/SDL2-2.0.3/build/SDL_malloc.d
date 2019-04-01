1d7be948ef400cc3bf7aa178c1b7c2e0963facbb
Cesar GLAVIEUX <cglavieu@e1r8p13.42.fr>
2019-04-01 21:47:26 +0200

    premier jet fonctionnel

--- cglavieu/SDL2-2.0.3/build/SDL_malloc.d
+++
@@ -1,7 +0,0 @@
-build/SDL_malloc.lo: \
-  /Users/cglavieu/Code/Doom-Nukem3D/engine/cglavieu/SDL2-2.0.3/src/stdlib/SDL_malloc.c \
-  /Users/cglavieu/Code/Doom-Nukem3D/engine/cglavieu/SDL2-2.0.3/src/stdlib/../SDL_internal.h \
-  /Users/cglavieu/Code/Doom-Nukem3D/engine/cglavieu/SDL2-2.0.3/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/cglavieu/Code/Doom-Nukem3D/engine/cglavieu/SDL2-2.0.3/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h
