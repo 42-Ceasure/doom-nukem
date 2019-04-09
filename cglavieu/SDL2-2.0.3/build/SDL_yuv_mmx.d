2c7ea002520f36b233afdc8d47dad764094aab5d
42-Ceasure <cglavieu@student.42.fr>
2019-04-09 13:48:56 +0200

    -

--- cglavieu/SDL2-2.0.3/build/SDL_yuv_mmx.d
+++
@@ -1,7 +0,0 @@
-build/SDL_yuv_mmx.lo: \
-  /Users/agay/Doom_Nukem/cglavieu/SDL2-2.0.3/src/render/SDL_yuv_mmx.c \
-  /Users/agay/Doom_Nukem/cglavieu/SDL2-2.0.3/src/render/../SDL_internal.h \
-  /Users/agay/Doom_Nukem/cglavieu/SDL2-2.0.3/src/render/../dynapi/SDL_dynapi.h \
-  /Users/agay/Doom_Nukem/cglavieu/SDL2-2.0.3/src/render/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
