3e550d31c33da952263e254b5d868b80c95b9063
Ceasure <cglavieu@Ceasures-iMac.local>
2019-05-10 10:46:22 +0200

    cleanup dev

--- ochaar/lib/SDL2-2.0.3/build/.libs/SDL_stdlib.d
+++
@@ -1,8 +0,0 @@
-build/SDL_stdlib.lo: \
-  /Users/ochaar/Documents/doom/cglavieu/SDL2-2.0.3/src/stdlib/SDL_stdlib.c \
-  /Users/ochaar/Documents/doom/cglavieu/SDL2-2.0.3/src/stdlib/../SDL_internal.h \
-  /Users/ochaar/Documents/doom/cglavieu/SDL2-2.0.3/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/ochaar/Documents/doom/cglavieu/SDL2-2.0.3/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h \
-  /Users/ochaar/Documents/doom/cglavieu/SDL2-2.0.3/src/stdlib/../libm/math_libm.h
--- ochaar/lib/SDL2_mixer-2.0.1/build/tables.d
+++
@@ -1,8 +0,0 @@
-build/tables.lo: timidity/tables.c timidity/config.h \
-  /Users/ochaar/Documents/doom/cglavieu/./libSDL2/include/SDL2/SDL_config.h \
-  /Users/ochaar/Documents/doom/cglavieu/./libSDL2/include/SDL2/SDL_platform.h \
-  /Users/ochaar/Documents/doom/cglavieu/./libSDL2/include/SDL2/begin_code.h \
-  /Users/ochaar/Documents/doom/cglavieu/./libSDL2/include/SDL2/close_code.h \
-  /Users/ochaar/Documents/doom/cglavieu/./libSDL2/include/SDL2/SDL_endian.h \
-  /Users/ochaar/Documents/doom/cglavieu/./libSDL2/include/SDL2/SDL_stdinc.h \
-  timidity/common.h timidity/tables.h timidity/instrum.h
