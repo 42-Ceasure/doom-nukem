79435088cf42b278af456dfbb84973eb936080eb
Nicolas VIENOT <nvienot@student.42.fr>
2019-07-18 18:39:56 +0200

    [NV] If god_mod == 1 && plafondplus haut que sol alors modif des hauteurs pageup

--- src/key_events.c
+++ src/key_events.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   key_events.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ochaar <ochaar@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nvienot <nvienot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 15:04:59 by ochaar            #+#    #+#             */
-/*   Updated: 2019/07/18 10:27:53 by ochaar           ###   ########.fr       */
+/*   Updated: 2019/07/18 18:38:41 by nvienot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 void		move_floor_ceiling(t_env *w, t_map *m)
 {
-	if (w->inkeys[SDL_SCANCODE_PAGEUP])
-		m->sector[m->player.sector].floor += 0.1;
-	if (w->inkeys[SDL_SCANCODE_PAGEDOWN])
+	if (w->inkeys[SDL_SCANCODE_PAGEUP] && m->god_mod == 1)
+		{
+			if (m->sector[m->player.sector].ceiling > m->sector[m->player.sector].floor + 3)
+				m->sector[m->player.sector].floor += 0.1;
+		}
+	if (w->inkeys[SDL_SCANCODE_PAGEDOWN] && m->god_mod == 1)
 		m->sector[m->player.sector].floor -= 0.1;
-	if (w->inkeys[SDL_SCANCODE_HOME])
+	if (w->inkeys[SDL_SCANCODE_HOME] && m->god_mod == 1)
 		m->sector[m->player.sector].ceiling += 0.1;
-	if (w->inkeys[SDL_SCANCODE_END])
-		m->sector[m->player.sector].ceiling -= 0.1;
+	if (w->inkeys[SDL_SCANCODE_END] && m->god_mod == 1)
+	{
+		if (m->sector[m->player.sector].floor < m->sector[m->player.sector].ceiling - 3)
+			m->sector[m->player.sector].ceiling -= 0.1;
+	}
 	if (m->sector[m->player.sector].texturing[2] == 6 && KEY == SDLK_f
 		&& m->player.take[3] == 1 && m->elevator == 0)
 	{
